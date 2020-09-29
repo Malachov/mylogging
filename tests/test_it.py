cdac63b4ac5c64e4095912f341bab4c1d43ee4e4
Malachov <malachovd@seznam.cz>
2020-09-29 13:15:26 +0200

    New version

--- tests/test_it.py
+++ tests/test_it.py
@@ -16,9 +16,9 @@ sys.path.insert(0, Path(os.path.abspath(inspect.getframeinfo(inspect.currentfram
 
 def test_warnings():
 
-    from mylogging import mylogger
+    import mylogging
 
-    mylogger._COLORIZE = 0  # Turn on colorization on all functions
+    mylogging._COLORIZE = 0  # Turn on colorization on all functions
 
     # We can define whether to
     #   display warnings: debug=1,
@@ -26,25 +26,24 @@ def test_warnings():
     #   stop warnings as errors: debug=3
     # Beware, that even if no warnings are configured, default warning setings are applied - so warning settings can be overwriten
 
-    mylogger.set_warnings(debug=1, ignored_warnings=["invalid value encountered in sqrt",
-                                                     "encountered in double_scalars"])
+    mylogging.set_warnings(debug=1, ignored_warnings=["invalid value encountered in sqrt",
+                                                      "encountered in double_scalars"])
 
     # We can create warning that will be displayed based on warning settings
-    mylogger.user_warning('Hessian matrix copmputation failed for example', caption="RuntimeError on model x")
+    mylogging.user_warning('Hessian matrix copmputation failed for example', caption="RuntimeError on model x")
 
     # In case we don't know exact error reason, we can use traceback_warning in try/except block
 
     try:
-        u = 10 / 0
+        print(10 / 0)
 
     except Exception:
-        mylogger.traceback_warning("Maybe try to use something different than 0")
+        mylogging.traceback_warning("Maybe try to use something different than 0")
 
     # In case we don't want to warn, but we have error that should be printed anyway and not based on warning settings, we can use user_message that return extended that we can use...
 
-    print(mylogger.user_message("I will be printed anyway"))
+    print(mylogging.user_message("I will be printed anyway"))
 
-# if __name__ == "__main__":
-#     # test_print_warning()
-#     test_user_warning()
-#     pass
+
+if __name__ == "__main__":
+    test_warnings()
